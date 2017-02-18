92b0f0166663e86fbbd0be1d2fa5ed009bc62043
Authorty <itismejody@GMAIL.COM>
2017-02-18 08:24:53 -0600

    Test

--- IRCListener/Program.cs
+++ IRCListener/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,12 @@ namespace IRCListener
     {
         static void Main(string[] args)
         {
+            //set the variables
+            //make the connection to d2jsp
+            while (true)
+            {
+                File.Create("C:/Testing.txt");
+            }
         }
     }
 }
